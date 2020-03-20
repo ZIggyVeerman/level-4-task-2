c5c6b246626fdd2ba69e7bbffd5ddb8c1e55b9e1
Ziggy Veerman <ziggy.veerman@hva.nl>
2020-03-20 15:56:11 +0100

    added back button

--- app/src/main/java/com/example/rockpaperscissors/activities/History.kt
+++ app/src/main/java/com/example/rockpaperscissors/activities/History.kt
@@ -6,20 +6,40 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import com.example.rockpaperscissors.R
+import com.example.rockpaperscissors.repositories.GameRepository
 import kotlinx.android.synthetic.main.activity_history.*
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class History : AppCompatActivity() {
+  private lateinit var gameRepository: GameRepository
+  private val mainScope = CoroutineScope(Dispatchers.Main)
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_history)
     setSupportActionBar(toolbarHistory)
     supportActionBar?.title = "Game History"
+    supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+    gameRepository = GameRepository(this)
+
+    initViews()
+
+  }
+
+  private fun initViews(){
+
   }
 
   private fun deleteHistory(){
 
+  }
+
+  private fun getGames() {
+//    mainScope.launch {  }
   }
 
   override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -35,6 +55,10 @@ class History : AppCompatActivity() {
     return when (item.itemId) {
       R.id.ic_delete_white -> {
         deleteHistory()
+        true
+      }
+      android.R.id.home -> {
+        finish()
         true
       }
       else -> super.onOptionsItemSelected(item)
